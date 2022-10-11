a5eaaa9e4941b34fb559ffec5a88a18390d42c58
Jakub Drożdż <jakubekso@outlook.com>
2022-10-11 09:56:15 +0200

    database connection working

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,22 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        //Connection working
+        /*MySqlCredentials msc = new MySqlCredentials();
+        try (Connection conection = DriverManager.getConnection(msc.getConnectionUrl(), msc.getUsername(), msc.getPass())) {
+            String statemnt = "INSERT INTO users VALUES(null,'Adam','Kowalski','96122145537','02-285','Warszawa','akowlaski@outlook.com')";
+            Statement stmt = conection.createStatement();
+            stmt.executeUpdate(statemnt);
+            System.out.println("Row inserted!");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }*/
     }
 }
