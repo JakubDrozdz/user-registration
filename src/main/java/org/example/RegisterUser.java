1381cfc5124ca141904cc02cb48d754868bc97d7
Jakub Drożdż <jakubekso@outlook.com>
2022-10-11 10:47:01 +0200

    working user registration

--- src/main/java/org/example/RegisterUser.java
+++ src/main/java/org/example/RegisterUser.java
@@ -1,9 +1,19 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class RegisterUser {
     private static final Scanner scan = new Scanner(System.in);
+    private MySqlCredentials msc;
+
+    public RegisterUser() {
+        msc = new MySqlCredentials();
+    }
+
     public void register(){
         DataValidation dv = new DataValidation();
         System.out.print("Enter first name:");
@@ -16,16 +26,39 @@ public class RegisterUser {
         String postalCode = scan.nextLine();
         while(!dv.validatePostalCode(postalCode)){
             System.out.println("Enter correct postal code!");
+            postalCode = scan.nextLine();
         }
         System.out.print("Enter pesel:");
         String pesel = scan.nextLine();
         while(!dv.validatePesel(pesel)){
             System.out.println("Enter correct pesel!");
+            pesel = scan.nextLine();
         }
         System.out.print("Enter e-mail:");
         String email = scan.nextLine();
         while(!dv.validateEmail(email)){
             System.out.println("Enter correct e-mail!");
+            email = scan.nextLine();
+        }
+
+        User newUser = new User(firstName,lastName,pesel,postalCode,city,email);
+        saveToDb(newUser);
+    }
+
+    private void saveToDb(User user){
+        try (Connection conection = DriverManager.getConnection(msc.getConnectionUrl(), msc.getUsername(), msc.getPass())) {
+            String statemnt = "INSERT INTO users VALUES(null," +
+                    "'"+user.getFirstName()+"'," +
+                    "'"+user.getLastName()+"'," +
+                    "'"+user.getPesel()+"'," +
+                    "'"+user.getPostalCode()+"'," +
+                    "'"+user.getCity()+"'," +
+                    "'"+user.getEmail()+"')";
+            Statement stmt = conection.createStatement();
+            stmt.executeUpdate(statemnt);
+            System.out.println("Row inserted!");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -10,11 +10,13 @@ public class User {
 
     public User() {}
 
-    public User(String firstName, String lastName, String postalCode, String city) {
+    public User(String firstName, String lastName, String pesel, String postalCode, String city, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.pesel = pesel;
         this.postalCode = postalCode;
         this.city = city;
+        this.email = email;
     }
 
     public String getFirstName() {return firstName;}
@@ -40,4 +42,16 @@ public class User {
     public String getEmail() {return email;}
 
     public void setEmail(String email) {this.email = email;}
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", pesel='" + pesel + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", city='" + city + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
